f5fbb15c511411137729c5e96673ec8c636ea154
James Prewett <download@carc.unm.edu>
2021-01-15 14:40:38 -0700

    Initial commit.

---
+++ dagon.lisp
@@ -0,0 +1,46 @@
+
+(defpackage dagon
+  (:use :common-lisp))
+
+(in-package :dagon)
+
+(ql:quickload :cl-fad)
+
+					;(declaim (optimize (space 3) (debug 3)))
+(declaim (optimize (debug 1) (speed 3)))
+
+(defun zero-size (l)
+  (eql 0 (car (last l))))
+
+(defun lf (file &key (time-type 'atime))
+  "return a list of file time and size"
+  (ignore-errors         ; in case there is an error finding the file
+    (let ((stat (sb-posix:stat file))
+	  (time-type (intern (symbol-name time-type) (find-package 'sb-posix))))
+      (list
+       (slot-value stat time-type) 
+       (slot-value stat 'sb-posix::size)))))
+
+(defun list-file (file &key (time-type 'atime))
+  "return a list of filename, its time, and size"
+  (let ((found (lf file :time-type time-type)))
+    (if found
+	(cons file found))))
+
+(defun list-dir (directory &key (filenames t) (time-type 'atime))
+  (multiple-value-bind (files directories)
+	(loop  for e in (cl-fad:list-directory directory)
+	   when (cl-fad:directory-exists-p e) collect e into dirs
+	   else collect e into files
+	   finally (return (values files dirs)))
+      (nconc
+       (remove nil
+	       (if filenames
+		   (loop for file in files
+		      collect (list-file file :time-type time-type))
+		   (loop for file in files collect (lf file :time-type time-type))))
+       (loop for found-dir in directories nconc
+	    (nconc (list-dir found-dir :time-type time-type :filenames filenames))))))
+
+(defun tot-size (l)
+  (loop for f in l sum (car (last f))))
