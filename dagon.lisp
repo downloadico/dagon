74264d48f157cfba5c90bb1705f5b1cffd85024d
James Prewett <download@carc.unm.edu>
2022-03-24 16:49:06 -0600

    changed dagon to output JSON.
    top-level is now called DAGON.

--- dagon.lisp
+++ dagon.lisp
@@ -1,103 +1,62 @@
+(mapcar #'ql:quickload '( :hunchentoot :cl-who :parenscript :cl-fad :cl-json))
 
 (defpackage dagon
-  (:use :common-lisp))
+  (:use :common-lisp :hunchentoot :cl-who :parenscript))
 
 (in-package :dagon)
 
-(ql:quickload :cl-fad)
-
-;;(declaim (optimize (space 3) (debug 1)))
-(declaim (optimize (debug 1) (space 2) (speed 3)))
+;; (declaim (optimize (space 3) (debug 1)))
+;; (declaim (optimize (debug 3) (speed 0)))
+(declaim (optimize (debug 1) (speed 3)))
 
 (defmacro with-file-stat ((stat-var file) &body body)
   `(let ((,stat-var (sb-posix:stat ,file)))
      ,@body))
 
-(defvar *chunk-size* 600)
-(defvar *ht-size* 4096)
-(defvar *ht* (make-hash-table :size *ht-size*))
+(defmacro with-unix-stat (file &body body)
+   `(multiple-value-bind (found-p st_dev st_ino st_mode st_nlink st_uid st_gid st_rdev st_size st_atime st_mtime st_ctime st_blksize st_blocks st_flags)
+	(sb-unix:unix-stat ,file)
+      ,@body))
 
-(defun reset ()
-  (setf *chunk-size* 600)
-  (setf *ht-size* 4096)
-  (setf *ht* (make-hash-table :size *ht-size*))
-  (sb-ext:gc :full t))
+(defclass my-stat ()
+  ((file :initarg :file)
+   (uid :initarg :uid)
+   (gid  :initarg :gid)
+   (size  :initarg :size)
+   (ctime :initarg :ctime)
+   (mtime :initarg :mtime)))
 
-(defun round-time (n)
-  (- n (mod n *chunk-size*)))
+(defun file-info (file)
+  (with-unix-stat (format nil "~A" file)
+    (if found-p
+	`(,(make-instance 'my-stat :file (format nil "~A" file) :uid st_uid  :gid st_gid :size st_size :mtime st_mtime :ctime st_ctime)))))
 
-(defun condense-table (ht chunk-size)
-  (warn "condensing ht chunk-size: ~A" chunk-size)
-  (let ((new-ht (make-hash-table :size *ht-size*)))
-    (loop for k being the hash-keys in ht
-       do
-	 (incf (gethash
-		(- k (mod k chunk-size))
-		new-ht 0)
-	       (gethash k ht)))
-    new-ht))
+(defun resolve-pathname (path)
+  (uiop/filesystem:resolve-symlinks path))
 
-(defun maybe-squeeze (ht)
-  (when (< .8 (/ (hash-table-count ht) (hash-table-size ht)))
-    (progn
-      (format t "current usage: ~A~%" (/ (hash-table-count ht) (hash-table-size ht)))
-      (format t "before condense ~A/~A~%" (hash-table-count ht) (hash-table-size ht))
-      
-      (setf ht
-	    (condense-table ht *chunk-size*))
-      (format t "Now ~A entries of ~A~%chunk-size: ~A~%"
-	      (hash-table-count ht)
-	      (hash-table-size ht)
-	      *chunk-size*)
-      (when  (< .8 (/ (hash-table-count ht) (hash-table-size ht)))
-	(setf *chunk-size* (* 10 *chunk-size*))
-	(format t "larger chunk ~A~%" *chunk-size*)
-	(setf ht
-	      (condense-table ht *chunk-size*))
-	)
-      
-      (when  (< .5 (/ (hash-table-count ht) (hash-table-size ht)))
-	(setf *ht-size* (* 2 *ht-size*))
-	(format t "larger size ~A~%" *ht-size*)
-	(setf ht
-	      (condense-table ht *chunk-size*))
-	)
-      
-	      
-      (format t "Now ~A entries of ~A~%chunk-size: ~A~%"
-	      (hash-table-count ht)
-	      (hash-table-size ht)
-	      *chunk-size*)
-      (sb-ext:gc :full t)
-      ;(room)
-      ))
-  ht)
+(defun dagon (path &optional (output "/tmp/dagon.out"))
+  (with-open-file (*standard-output* output
+				     :direction :output
+				     :if-exists :overwrite
+				     :if-does-not-exist :create)
+    (process-dir path)))
 
-(defun store-size-and-atime (file &optional (time-transform #'round-time))
-  (ignore-errors
-    (with-file-stat (stat file)
-      (with-slots ((size sb-posix::size)
-		   (time sb-posix::atime))
-	  stat
-	(when (< .5 (/ (hash-table-count *ht*) (hash-table-size *ht*)))
-	  (setf *ht*  (maybe-squeeze *ht*)))
-	(let ((transformed-time (funcall time-transform time)))
-	  (incf (gethash transformed-time *ht* 0) size))))))
-
-(defun process-file (file &key (fn #'store-size-and-atime))
-  "Apply fn to named file"
-  (funcall fn file))
-
-(defun keys (ht)
-  (loop for k being the hash-keys in ht collect k))
-
-(let ((*unix-epoch-difference* (encode-universal-time 0 0 0 1 1 1970 0)))
-  (defun universal-to-unix-time (universal-time)
-    (- universal-time *unix-epoch-difference*))
-
-  (defun unix-to-universal-time (unix-time)
-    (+ unix-time *unix-epoch-difference*))
-
-  (defun get-unix-time ()
-    (universal-to-unix-time (get-universal-time)))
-)
+(let ((crumbs (make-hash-table :test #'equal)))
+  (defun process-dir (directory)
+    (let ((directory (resolve-pathname directory)))
+      (if (gethash directory crumbs)
+	(warn "not entering ~A~%" directory)
+	(progn
+	  (setf (gethash directory crumbs) t)
+	  (warn "entering dir ~A" directory)
+	  (let ((dirents (cl-fad:list-directory
+			  directory
+			  :follow-symlinks nil)))
+	    (loop for ent in dirents when (and (cl-fad:directory-pathname-p ent)
+					       (not (gethash (resolve-pathname ent) crumbs)))
+		  do (process-dir ent))
+	    (loop for js
+		    in
+		    (loop for ent in dirents nconc (file-info ent)) do
+		      (format t "~A~%" (json:encode-json-to-string js))
+		  )))))))
